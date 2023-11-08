124a5f960e2e3349858a687b8f465aaef28f7064
syifanalfiah <112307944+syifanalfiah@users.noreply.github.com>
2023-11-08 22:52:27 +0700

    Update post.js

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const Post = new mongoose.Schema({
   name: { type: String, required: true },
   prompt: { type: String, required: true },
   photo: { type: String, required: true },
-});
+})
 
-const PostSchema = mongoose.model('Post', Post);
+// model of the schema
+const PostSchema = mongoose.model("Post", Post); //"Post" is the name of the model/schema. The second Post is the schema we just created.
 
 export default PostSchema;
