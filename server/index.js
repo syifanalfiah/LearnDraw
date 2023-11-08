1d3223e9047f80ec2c8af348ae60dadcfb52f8ec
syifanalfiah <112307944+syifanalfiah@users.noreply.github.com>
2023-11-08 22:51:15 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,33 +1,39 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import * as dotenv from 'dotenv'; //gives us access to our environment variables
+import cors from 'cors'; //allows us to make requests from our frontend to our backend
 
 import connectDB from './mongodb/connect.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 import postRoutes from './routes/postRoutes.js';
-import learndrawRoutes from './routes/learndrawRoutes.js';
 
-dotenv.config();
+dotenv.config(); //allows us to pull our environment variables from our .env file
 
+//initialize express application 
 const app = express();
+
+//middleware
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json( { limit: '50mb' } ));
 
+//Creates API endpoints that we can connect to from our Frontend
+app.use('/api/v1/dalle', dalleRoutes); 
 app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/learndraw', learndrawRoutes);
 
+//routes
+//root route
 app.get('/', async (req, res) => {
-  res.status(200).json({
-    message: 'This Is LearnDraw!',
-  });
-});
+  res.send('Hello from Dall-E!');
+}); //this ensures that our server is running once we visit the URL of our server
 
+//to run server
 const startServer = async () => {
+  //connect to DB
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
+      app.listen(8080, () => console.log('Server has started on port http://localhost:8080'));
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
-};
+}
 
 startServer();
